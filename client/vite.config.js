59a6d4d619d8cf64a1206f11efde08ec09045cc3
Junaid Khan <pathanjunaid7888@gmail.com>
2025-06-23 01:48:47 +0530

    Added base config for Vercel deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // Important for relative path resolution
   plugins: [
     react(),
-  tailwindcss(),
-],
+    tailwindcss(),
+  ],
 })
