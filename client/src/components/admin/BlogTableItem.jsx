7e7cb872da9411c7551227d9714c19b2594179fa
Junaid Khan <pathanjunaid7888@gmail.com>
2025-07-11 18:10:24 +0530

    Added New AI feature, More Optimization

--- client/src/components/admin/BlogTableItem.jsx
+++ client/src/components/admin/BlogTableItem.jsx
@@ -8,7 +8,7 @@ const BlogTableItem = ({blog, fetchBlogs, index}) => {
 
 
 
-    const {title, createdAt} = blog;
+    const {title, createdAt, _id} = blog;
     const BlogDate = new Date(createdAt);
 
     const {axios, backendUrl, navigate} = useContext(AppContent)
@@ -61,7 +61,7 @@ const BlogTableItem = ({blog, fetchBlogs, index}) => {
     <>
     <tr className='border-y border-gray-300  '>
       <th className=' py-4 sm:px-2 sm:py-4 '>{ index }</th>
-      <td onClick={()=>navigate('/blog-x')} className='py-4 sm:px-2 sm:py-4 cursor-pointer'>{ title }</td>
+      <td onClick={()=>navigate(`/blog/${_id}`)} className='py-4 sm:px-2 sm:py-4 cursor-pointer'>{ title }</td>
       <td className='px-2 py-4 max-sm:hidden'>{ BlogDate.toDateString() }</td>
       <td className='px-2 py-4 max-sm:hidden'>
         <p className={`${blog.isPublished ? "text-green-600" : "text-orange-700"}`}>
